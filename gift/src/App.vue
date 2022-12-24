154b2edbb3e906f172fd366b7fe8ea6be4cb1b7d
linerlin <71690181+linerlini@users.noreply.github.com>
2022-12-24 21:14:43 +0800

    Add files via upload

---
+++ gift/src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Gift from './components/Gift.vue'
+import TreeVue from './components/Tree.vue';
+import Card from './components/Card.vue'
+const treeVisibleClass = ref('tree--hidden')
+const cardOffsetClass = ref('card--hidden')
+const handleClickGift = () => {
+  treeVisibleClass.value = 'tree--visible'
+  cardOffsetClass.value = 'card--visible'
+}
+</script>
+
+<template>
+  <div class="page">
+    <Gift @click="handleClickGift" class="test"></Gift>
+    <TreeVue :class="treeVisibleClass"></TreeVue>
+    <Card :class="cardOffsetClass"></Card>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  background-color: var(--blue);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.tree--visible {
+  transition: 500ms opacity ease-in-out;
+  transition-delay: 250m;
+  opacity: 1;
+  z-index: 0;
+}
+.tree--hidden {
+  opacity: 0;
+  z-index: -1;
+}
+.card--hidden {
+  transform: translate(-50%, 100%);
+}
+.card--visible {
+  transform: translate(-50%, 50%);
+  transition: transform 500ms ease-in-out;
+}
+</style>
---
+++ gift/src/components/Card.vue
@@ -0,0 +1,167 @@
+<template>
+  <div class="card__wrapper">
+    <div class="card__text">
+      <p>Merry</p>
+      <p>Christmas!</p>
+    </div>
+    <div class="card">
+      <div class="snow"></div>
+      <div class="snow"></div>
+
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+      <div class="snow"></div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+.card__wrapper {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  .card {
+    $snow-size: 2em;
+    width: $snow-size * 10;
+    height: $snow-size * 10;
+    padding: 2em;
+    display: flex;
+    flex-wrap: wrap;
+    filter: blur(15px);
+
+    .snow {
+      position: relative;
+      width: $snow-size;
+      height: $snow-size;
+      border-radius: 100%;
+      filter: contrast(30);
+
+      &::after {
+        position: absolute;
+        content: "";
+        left: 0;
+        width: inherit;
+        height: inherit;
+        background: white;
+        transition: 30s cubic-bezier(0.19, 1, 0.22, 1);
+      }
+
+      &:hover::after {
+        transform: scale(0);
+        transition-duration: 0.1s;
+      }
+    }
+  }
+
+  .card__text {
+    position: absolute;
+    top: 25%;
+    left: 50%;
+    z-index: 9999;
+    color: white;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 1em;
+    letter-spacing: 0.1em;
+    transform: translate(-50%, -30%);
+    pointer-events: none;
+    filter: blur(1px) contrast(20);
+    text-align: center;
+    p {
+      margin-bottom: 12px;
+    }
+  }
+}
+</style>
